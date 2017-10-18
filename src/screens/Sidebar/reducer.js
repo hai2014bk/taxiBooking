4d5b977be1cc72c136619dc26de4616921d386a8
QuangTrung-PC\Quang Trung <quangtrung2503@gmail.com>
2017-10-18 15:18:18 +0700

    fix bug

---
+++ src/screens/Sidebar/reducer.js
@@ -0,0 +1,21 @@
+const initialState = {
+  sidebarDisabled: true,
+  kind: "close",
+};
+export type State ={
+  kind: string,
+  sidebarDisabled: boolean,
+}
+export default function(state: State = initialState, action: Function) {
+  console.log(":qqqqqqqqqqqqqqqqqqq")
+  switch (action.type) {
+    case "OPEN_SIDEBAR":
+      return { ...state, kind: "open", };
+    case "CLOSE_SIDEBAR": 
+      return { ...state, kind: "close" };
+    case "RELOAD_SIDEBAR":
+      return { ...state, kind: "reload", };
+    default:
+      return state;
+  }
+}
